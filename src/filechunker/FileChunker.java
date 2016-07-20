b8a9095f204398ab21237d7b9d6362793dbc3a02
Travis <travjones0@gmail.com>
2016-07-20 13:24:46 -0400

    update

--- src/filechunker/FileChunker.java
+++ src/filechunker/FileChunker.java
@@ -10,39 +10,6 @@ public class FileChunker {
     // chunk size 100KB
     public static long chunkSize = 100000;
 
-//    public static void main(String[] args) throws Exception
-//    {
-//        if (args.length != 2)
-//        {
-//            System.out.println("Must specify a flag -split or -join and a file argument. The file argument for splitting is the file to split and for joining is the base filename to join on.");
-//            System.exit(0);
-//        }
-//
-//        try
-//        {
-//            if (args[0].equalsIgnoreCase("-split"))
-//                split(args[1]);
-//            else if (args[0].equalsIgnoreCase("-join"))
-//                join(args[1]);
-//            else
-//            {
-//                System.out.println("The first argument must be an option:");
-//                System.out.println("\t-split: split the specified file");
-//                System.out.println("\t-join: join all splitter outfiles with the specified base filename");
-//                System.exit(0);
-//            }
-//        }
-//        catch (FileNotFoundException e)
-//        {
-//            System.out.println("File not found: " + args[1]);
-//        }
-//        catch (IOException e)
-//        {
-//            System.out.println("IO Error");
-//        }
-//    }
-
-
     public static void split(String filename) throws IOException {
         // open file
         BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
@@ -54,7 +21,7 @@ public class FileChunker {
         int chunkID;
         for (chunkID = 0; chunkID < fileSize / chunkSize; chunkID++) {
             // open output file
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + chunkID));
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("./server_data/" + filename + "." + chunkID));
 
             // write bytes to file
             for (int currentByte = 0; currentByte < chunkSize; currentByte++) {
@@ -67,7 +34,7 @@ public class FileChunker {
         // last chunk might be smaller than chunk size
         if (fileSize != chunkSize * (chunkID - 1)) {
             // open output file
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + chunkID));
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("./server_data/" + filename + "." + chunkID));
 
             int b;
             while ((b = in.read()) != -1) {
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,14 +2,33 @@ package server;
 
 import filechunker.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by travis on 7/18/16.
  */
 public class Server {
     public static void main(String args[]) throws IOException {
+        System.out.println("Server is up!");
+
+        // create dir for server chunks
+        Path path = Paths.get("server_data");
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                //fail to create directory
+                e.printStackTrace();
+            }
+        }
+
+        // first CLI argument filename to split
         FileChunker fc = new FileChunker();
         fc.split(args[0]);
+
     }
 }
