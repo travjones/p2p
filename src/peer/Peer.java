6fde528f87dcec2e0e6b320d4f39e700d4a66728
Travis <travjones0@gmail.com>
2016-07-22 16:43:45 -0400

    first round of p2p xfer

--- src/peer/Peer.java
+++ src/peer/Peer.java
@@ -1,13 +1,13 @@
 package peer;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by travis on 7/18/16.
@@ -22,6 +22,8 @@ public class Peer {
     private int peerULID;
     private int numInitialChunks;
     private int numChunks;
+    private int peerULport;
+    private int peerDLport;
 
     private void run() throws ClassNotFoundException, IOException {
         try {
@@ -34,22 +36,19 @@ public class Peer {
 
             // set peerID from server input
             peerID = (int) in.readObject();
-            System.out.println(peerID);
 
             // set numInitialChunks from server input
             numInitialChunks = (int) in.readObject();
-            System.out.println(numInitialChunks);
 
             // set numChunks from server input
             numChunks = (int) in.readObject();
-            System.out.println(numChunks);
 
             // read config
             String[] config = readConfig(peerID);
-            System.out.println(Arrays.toString(config));
             peerPort = Integer.parseInt(config[1]);
             peerDLID = Integer.parseInt(config[2]);
             peerULID = Integer.parseInt(config[3]);
+            peerDLport = Integer.parseInt(config[5]);
 
             // create folder to hold peer chunks
             Path path = Paths.get("peer" + peerID + "_data");
@@ -69,14 +68,14 @@ public class Peer {
             ServerSocket ss = new ServerSocket(peerPort);
             System.out.println("Peer is listening on " + peerPort);
             System.out.println("Waiting for peer " + (Integer.parseInt(config[2]) - 1) + " to connect...");
+            new PeerDLHandler(requestSocket, peerDLport, peerID).start();
             try {
                 while (true) {
-                    new PeerULHandler(ss.accept(), peerID, peerPort);
+                    new PeerULHandler(ss.accept(), peerID, peerPort).start();
                 }
             } finally {
                 ss.close();
             }
-
 //            int bytesRead;
 //            int current = 0;
 //            FileOutputStream fos;
@@ -128,11 +127,56 @@ public class Peer {
         }
 
         public void run() {
-            // write this
+            System.out.println("Peer connected on " + pPort + "...");
+            try {
+                ArrayList<String> chunksToSend;
+                chunksToSend = sendChunkList(connection, peerID);
+                sendChunks(chunksToSend, peerID, connection);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private static class PeerDLHandler extends Thread {}
+    private static class PeerDLHandler extends Thread {
+        private Socket requestSocket;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+        private int pDLPort;
+        private int peerID;
+
+        PeerDLHandler(Socket requestSocket, int pDLPort, int peerID) {
+            this.requestSocket = requestSocket;
+            this.pDLPort = pDLPort;
+            this.peerID = peerID;
+        }
+
+        public void run() {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        requestSocket = new Socket("localhost", pDLPort);
+                        System.out.println("Connected to UL peer on " + pDLPort);
+                        timer.cancel();
+                        timer.purge();
+
+                        ArrayList<String> chunks;
+                        chunks = (ArrayList<String>) receiveChunkList(requestSocket, peerID);
+                        receiveChunks(peerID, requestSocket);
+                    } catch (IOException e) {
+                        System.out.println("Could not connect to UL peer...");
+//                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 0, 1000);
+        }
+    }
 
     private static String[] readConfig(int peerID) throws IOException {
         // read in config
@@ -153,8 +197,7 @@ public class Peer {
         int filesCount = dis.readInt();
         File[] files = new File[filesCount];
 
-        for(int i = 0; i < filesCount; i++)
-        {
+        for (int i = 0; i < filesCount; i++) {
             long fileLength = dis.readLong();
             String fileName = dis.readUTF();
 
@@ -163,7 +206,7 @@ public class Peer {
             FileOutputStream fos = new FileOutputStream(files[i]);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-            for(int j = 0; j < fileLength; j++) bos.write(bis.read());
+            for (int j = 0; j < fileLength; j++) bos.write(bis.read());
 
             bos.close();
         }
@@ -180,6 +223,127 @@ public class Peer {
             pw.println(file.getName());
         }
         pw.close();
+    }
+
+    public static ArrayList<String> sendChunkList(Socket connection, int peerID) throws IOException, ClassNotFoundException {
+        String filename = "./peer" + peerID + "summary.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+
+        List<String> chunkList = new ArrayList<>();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            chunkList.add(line);
+//            String[] shards = line.split("\\.");
+//            chunkList.add(shards[2]);
+        }
+        br.close();
+
+
+        BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.flush();
+        oos.writeObject(chunkList);
+        oos.flush();
+
+        BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        ArrayList<String> chunksToSend;
+        chunksToSend = (ArrayList<String>) ois.readObject();
+        return chunksToSend;
+    }
+
+    public static List<String> receiveChunkList(Socket requestSocket, int peerID) throws IOException, ClassNotFoundException {
+        ArrayList<String> chunkList;
+        BufferedInputStream bis = new BufferedInputStream(requestSocket.getInputStream());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        chunkList = (ArrayList<String>) ois.readObject();
+        System.out.println("Received chunk list: " + Arrays.toString(chunkList.toArray()));
+
+        String filename = "./peer" + peerID + "summary.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+
+        List<String> chunksToGetList = new ArrayList<>();
+        List<String> myChunkList = new ArrayList<>();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            myChunkList.add(line);
+//            String[] shards = line.split("\\.");
+//            myChunkList.add(shards[2]);
+        }
+        br.close();
+
+        for (String el : chunkList) {
+            if (!myChunkList.contains(el)) {
+                chunksToGetList.add(el);
+            }
+        }
+        System.out.println("My chunk list: " + Arrays.toString(myChunkList.toArray()));
+        System.out.println("Chunks to request: " + Arrays.toString(chunksToGetList.toArray()));
+
+        BufferedOutputStream bos = new BufferedOutputStream(requestSocket.getOutputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.flush();
+        oos.writeObject(chunksToGetList);
+        oos.flush();
+
+        return chunksToGetList;
+
+//        chunksToGetList.addAll(chunkList.stream().filter(myChunkList::contains).collect(Collectors.toList()));
+    }
+
+    public static void sendChunks(ArrayList<String> reqChunks, int peerID, Socket connection) throws IOException {
+        String dir = "./peer" + peerID + "_data";
+
+        BufferedOutputStream bos = new BufferedOutputStream(connection.getOutputStream());
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        dos.writeInt(reqChunks.size());
+        for (String chunk : reqChunks) {
+            File fileChunk = new File(dir + "/" + chunk);
+            long length = fileChunk.length();
+            dos.writeLong(length);
+
+            String name = fileChunk.getName();
+            dos.writeUTF(name);
+
+            FileInputStream fis = new FileInputStream(fileChunk);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            int b = 0;
+            while ((b = bis.read()) != -1) bos.write(b);
+
+            bis.close();
+        }
+        dos.close();
+    }
+
+    public static void receiveChunks(int peerID, Socket connection) throws IOException {
+        String dir = "./peer" + peerID + "_data";
+        BufferedInputStream bis = new BufferedInputStream(connection.getInputStream());
+        DataInputStream dis = new DataInputStream(bis);
+
+        int filesCount = dis.readInt();
+        File[] files = new File[filesCount];
+
+        for (int i = 0; i < filesCount; i++) {
+            long fileLength = dis.readLong();
+            String fileName = dis.readUTF();
+
+            files[i] = new File(dir + "/" + fileName);
+
+            FileOutputStream fos = new FileOutputStream(files[i]);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            for (int j = 0; j < fileLength; j++) bos.write(bis.read());
+
+            bos.close();
+        }
+
+        dis.close();
+
+        // update summary file
+        summaryFile(peerID);
+
     }
 
     public static void main(String[] args) throws ClassNotFoundException, IOException {
